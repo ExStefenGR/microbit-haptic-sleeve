c8d40274e393c4ece158a5049d850ecbd2373404
Stefanos T <sttqueries@gmail.com>
2022-11-28 18:34:57 +0000

    new changes

--- main.ts
+++ main.ts
@@ -1,42 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= -120) {
-        basic.showLeds(`
-            # # # # #
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        pins.analogWritePin(AnalogPin.P0, 125)
+        pins.analogWritePin(AnalogPin.P1, 128)
+    }
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= -80) {
+        pins.analogWritePin(AnalogPin.P0, 256)
+        pins.analogWritePin(AnalogPin.P1, 258)
     }
     if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= -70) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
+        pins.analogWritePin(AnalogPin.P0, 500)
+        pins.analogWritePin(AnalogPin.P1, 512)
     }
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= -50) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            . . . . .
-            `)
-    }
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= -42) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= -60) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+        pins.analogWritePin(AnalogPin.P1, 1023)
     }
 })
 radio.setGroup(1)
+radio.setTransmitPower(7)
 basic.forever(function () {
     radio.sendNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
 })
